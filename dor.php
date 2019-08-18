96f083494d0388bcf339abf6634c9f1a4e1cc697
ColdDark29 <53429191+ColdDark29@users.noreply.github.com>
2019-08-18 18:30:31 +0800

    dor.sh

--- dor.php
+++ dor.php
@@ -1,48 +1,33 @@
-<?php
+#!/bin/env bash
 #Author = Cahyadi
 #Upload date = 04-08-2019
 #Update = 14-08-2019(v2.0)
-#Update = 18-08-2019(v3.0)
-$r = "\033[0m";
-$m = "\033[31;1;5m";
-$h = "\033[32;1;5m";
-$k = "\033[33;1;5m";
-$b = "\033[34;1;5m";
-$c = "\033[36;1;5m";
-$p = "\033[37;1;5m";
-$logo =
-  $h."▀█▀ █▀▀ █▄░▄█ █▀▄ ▄▀▄ █░▄▀ ".$k." .         ,    
-".$h."░█░ █▀▀ █░█░█ █▀█ █▀█ █▀▄░ ".$k." ;_/. . _ -+- _.
-".$h."░▀░ ▀▀▀ ▀░░░▀ ▀▀░ ▀░▀ ▀░▀▀ ".$k." | \(_|(_) | (_]
-".$h."Author : ".$p."Cahyadi
-".$h."Github : ".$p."https://github.com/ColdDark29/
-".$h."Versi  : ".$p."3.0
-".$b."──────────────────────────────────────────────────\n".$r;
-function m(){
-    global $logo,$h,$m,$k,$p,$r;
-    system("clear");
-    echo $logo;
-    echo $h."[★]====[ ".$k."1".$m.".".$h." XL Axiata                     ]====[★]\n";
-    echo $h."[★]====[ ".$k."2".$m.".".$h." Three(3)                      ]====[★]\n";
-    echo $h."[★]====[ ".$k."3".$m.". Keluar                        ".$h."]====[★]\n".$r;
-    pm();
-};
-function pm(){
-    global $h,$m,$p,$k,$r;
-    echo $h."[?] Pilih ".$k."=> ".$p;
-    $x = trim(fgets(STDIN));
-    if ($x == 1):
-        include("XLAxiata.php");
-    elseif($x == 2):
-        include("Three.php");
-    elseif($x == 3):
-        echo $m."[!] Keluar\n".$r;
-        sleep(1);
-        exit();
-    else:
-        echo $m."[!] Pilihan tidak ada\n".$r;
-        pm();
-    endif;
-};
-m();
-?>
+Update = 18-08-2019(v3.0)
+clear
+echo "\033[32;1;5m▀█▀ █▀▀ █▄░▄█ █▀▄ ▄▀▄ █░▄▀ \033[33;1;5m.      ,       "
+echo "\033[32;1;5m░█░ █▀▀ █░█░█ █▀█ █▀█ █▀▄░ \033[33;1;5m;_/. . _ -+- _."
+echo "\033[32;1;5m░▀░ ▀▀▀ ▀░░░▀ ▀▀░ ▀░▀ ▀░▀▀ \033[33;1;5m| \(_|(_) | (_]"
+echo "\033[32;1;5mAuthor : \033[37;1;5mCahyadi"
+echo "\033[32;1;5mGithub : \033[37;1;5mhttps://github.com/ColdDark29/"
+echo "\033[32;1;5mVersi  : \033[37;1;5m3.0"
+echo "\033[34;1;5m──────────────────────────────────────────────────"$r
+echo "\033[32;1;5m[★]====[ \033[33;1;5m1\033[31;1;5m. \033[32;1;5mXL Axiata                     ]====[★]"
+echo "\033[32;1;5m[★]====[ \033[33;1;5m2\033[31;1;5m. \033[32;1;5mThree(3)                      ]====[★]"
+echo "\033[32;1;5m[★]====[ \033[33;1;5m3\033[31;1;5m. Keluar                        \033[32;1;5m]====[★]"
+echo -n "\033[32;1;5m[?] Pilih \033[33;1;5m=> \033[37;1;5m"
+read pilih;
+if [ $pilih -eq 1 ]
+then
+    php XlAxiata.php
+elif [ $pilih -eq 2 ]
+then
+    php Three.php
+elif [ $pilih -eq 3 ]
+then
+    echo "\033[31;1;5m[!] Keluar\033[0m";
+    exit
+else
+    echo "\033[31;1;5m[!] Pilihan tidak ada\033[0m"
+    sleep 1
+    sh dor.sh
+fi
